8c07e08d5c74087e279210eef7f96d8a3a606d0d
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-23 21:59:38 -0600

    reading README and pseduo coding a bit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+//Page creation:
 //const generateHTML = require("")
+
+//team profiles - constructors?
+//const Employee = require(./lib) - parent class
+//const Manager = require(./lib)
+//const Engineer = require(./lib)
+//const Intern = require(./lib)
+
