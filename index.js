c8aeaf539abc1079305ada5c27c68f7b3a0521e3
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-25 10:05:03 -0600

    adding the code that i worked with the as a group last night but don't feel comfortable using the code exactly since i join the team late

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ const Intern = require("./lib/Intern");
 //name, employee id, email, and office number
 //.then menu option to add enigneer or intern (switch)
 //select option prompt questions
+
+//where all response comes into
+const teamProfileArray = [];
+
 const addManager = [
   {
     type: "input",
@@ -36,7 +40,7 @@ const addManager = [
     message: "Please enter the ID of your team manager",
     validate: (userInput) => {
         if (isNaN(userInput) || userInput.trim().length === 0) {
-          console.log("Please enter a valid ID for office number!");
+          console.log("Please enter a valid ID for your team manager!");
           return false;
         } else {
           return true;
@@ -51,7 +55,7 @@ const addManager = [
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter the first name of your team manager!");
+        console.log("Please enter the email of your team manager!");
         return false;
       }
     },
@@ -87,8 +91,60 @@ const addEmployeeMenu = [
           }
         },
       },
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the first name of employee",
+        validate: (userInput) => {
+          if (userInput) {
+            return true;
+          } else {
+            console.log("Please enter the first name of your employee!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the ID of your employee",
+        validate: (userInput) => {
+            if (isNaN(userInput) || userInput.trim().length === 0) {
+              console.log("Please enter a valid ID for your employee!");
+              return false;
+            } else {
+              return true;
+            }
+          },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter the email of your employee",
+        validate: (userInput) => {
+          if (userInput) {
+            return true;
+          } else {
+            console.log("Please enter the email of your employee!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please enter the github username of your employee",
+        
+        validate: (userInput) => {
+          if (userInput) {
+            return true;
+          } else {
+            console.log("Please enter the email of your employee!");
+            return false;
+          }
+        },
+      },
 ]
-
 //menu
 //engineer/intern
 
@@ -119,3 +175,124 @@ const addEmployeeMenu = [
   
   // Function call to initialize app
   init();
+
+
+
+// function runApp() {
+//   createTeam();
+//       //Creating My Team: 
+//       function createTeam() {
+//           inquirer.prompt([{
+//               type: "list",
+//               message: "What type of employees are you liking to add to your team: Manager, Engineer, Intern?",
+//               name: "addEmployee",
+//               choices: ["Manager", "Engineer", "Intern", "I am finished adding team members."]
+//           }]).then(function(input){
+//               switch(input.addEmployee){
+//                   case "Manager":
+//                       createManager();
+//                       break;
+//                   case "Engineer":
+//                       createEngineer();
+//                       break;
+//                   case "Intern":
+//                       createIntern();
+//                   default:
+//                       createHtml();
+//               }
+//           })
+//       }
+  
+//       function createManager() {
+//           inquirer.prompt([{
+//               type: 'input',
+//               name: "managerName",
+//               message: "What is your Manager's Name? "
+//           },
+//           {
+//               type: 'input',
+//               name: "managerId",
+//               message: "What is your Manager's ID? "
+//           },
+//           {
+//               type: 'input',
+//               name: "managerEmail",
+//               message: "What is your Manager's email? "
+//           },
+//           {
+//               type: 'input',
+//               name: "managerOfficeNumber",
+//               message: "What is your Manager's Office Number? "
+//           }
+//       ]).then(answers => {
+//           const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+//           teamArray.push(manager);
+//           createTeam();
+//         });
+//       }
+//           function createEngineer() {
+//               inquirer.prompt([
+//                   {
+//                       type: 'input',
+//                       name: "engineerName",
+//                       message: "What is your Engineer's Name: ?"
+//                   },
+//                   {
+//                       type: 'input',
+//                       name: "engineerId",
+//                       message: "What is your Engineer's ID?"
+//                   },
+//                   {
+//                       type: 'input',
+//                       name: "engineerEmail",
+//                       message: "What is your Engineer's Email?"
+//                   },
+//                   {
+//                       type: 'input',
+//                       name: "engineerGithub",
+//                       message: "What is your Engineer's GitHub username?"
+//                   },
+//                   ]).then(answers => {
+//                       const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.managerOfficeNumber);
+//                       teamArray.push(engineer);
+//                       createTeam();
+//                     });
+//           }
+//           function createIntern() {
+//               inquirer.prompt([
+//                   {
+//                       type: 'input',
+//                       message: "What is your Intern's Name: ?",
+//                       name: "name"
+//                   },
+//                   {
+//                       type: 'input',
+//                       message: "What is your Intern's ID number? ",
+//                       name: "id"
+//                   },
+//                   {
+//                       type: 'input',
+//                       message: "What is your Intern's ID Email? ",
+//                       name: "email"
+//                   },
+//                   {
+//                       type: 'input',
+//                       message: "What is your Intern's ID Office Number? ",
+//                       name: "school"
+//                   },
+                  
+//                   ]).then(answers => {
+//                       const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school);
+//                       teamArray.push(intern);
+//                       createTeam();
+//                     });
+//           }
+  
+  
+//       }
+//   //this is the end of the function. The main function
+//   function createHtml() {
+//       fs.writeFile('./dist/test.html', (teamArray))
+//   }
+//   //the actually initialization of the function runApp
+//   runApp();
