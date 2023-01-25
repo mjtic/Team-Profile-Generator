ca6bde6f457346944a039220a0842dc8fc5fc900
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-25 12:32:05 -0600

    removed reference code and trying something new in the init() function

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ const addEmployeeMenu = [
   // TODO: Create a function to initialize app
   function init() {
     inquirer.prompt(addManager).then((response) => {
+      teamProfileArray.push(addManager);
       console.log(response),
       inquirer.prompt(addEmployeeMenu).then((response)=>{
         console.log(response)
@@ -178,121 +179,4 @@ const addEmployeeMenu = [
 
 
 
-// function runApp() {
-//   createTeam();
-//       //Creating My Team: 
-//       function createTeam() {
-//           inquirer.prompt([{
-//               type: "list",
-//               message: "What type of employees are you liking to add to your team: Manager, Engineer, Intern?",
-//               name: "addEmployee",
-//               choices: ["Manager", "Engineer", "Intern", "I am finished adding team members."]
-//           }]).then(function(input){
-//               switch(input.addEmployee){
-//                   case "Manager":
-//                       createManager();
-//                       break;
-//                   case "Engineer":
-//                       createEngineer();
-//                       break;
-//                   case "Intern":
-//                       createIntern();
-//                   default:
-//                       createHtml();
-//               }
-//           })
-//       }
   
-//       function createManager() {
-//           inquirer.prompt([{
-//               type: 'input',
-//               name: "managerName",
-//               message: "What is your Manager's Name? "
-//           },
-//           {
-//               type: 'input',
-//               name: "managerId",
-//               message: "What is your Manager's ID? "
-//           },
-//           {
-//               type: 'input',
-//               name: "managerEmail",
-//               message: "What is your Manager's email? "
-//           },
-//           {
-//               type: 'input',
-//               name: "managerOfficeNumber",
-//               message: "What is your Manager's Office Number? "
-//           }
-//       ]).then(answers => {
-//           const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-//           teamArray.push(manager);
-//           createTeam();
-//         });
-//       }
-//           function createEngineer() {
-//               inquirer.prompt([
-//                   {
-//                       type: 'input',
-//                       name: "engineerName",
-//                       message: "What is your Engineer's Name: ?"
-//                   },
-//                   {
-//                       type: 'input',
-//                       name: "engineerId",
-//                       message: "What is your Engineer's ID?"
-//                   },
-//                   {
-//                       type: 'input',
-//                       name: "engineerEmail",
-//                       message: "What is your Engineer's Email?"
-//                   },
-//                   {
-//                       type: 'input',
-//                       name: "engineerGithub",
-//                       message: "What is your Engineer's GitHub username?"
-//                   },
-//                   ]).then(answers => {
-//                       const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.managerOfficeNumber);
-//                       teamArray.push(engineer);
-//                       createTeam();
-//                     });
-//           }
-//           function createIntern() {
-//               inquirer.prompt([
-//                   {
-//                       type: 'input',
-//                       message: "What is your Intern's Name: ?",
-//                       name: "name"
-//                   },
-//                   {
-//                       type: 'input',
-//                       message: "What is your Intern's ID number? ",
-//                       name: "id"
-//                   },
-//                   {
-//                       type: 'input',
-//                       message: "What is your Intern's ID Email? ",
-//                       name: "email"
-//                   },
-//                   {
-//                       type: 'input',
-//                       message: "What is your Intern's ID Office Number? ",
-//                       name: "school"
-//                   },
-                  
-//                   ]).then(answers => {
-//                       const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school);
-//                       teamArray.push(intern);
-//                       createTeam();
-//                     });
-//           }
-  
-  
-//       }
-//   //this is the end of the function. The main function
-//   function createHtml() {
-//       fs.writeFile('./dist/test.html', (teamArray))
-//   }
-//   //the actually initialization of the function runApp
-//   runApp();
